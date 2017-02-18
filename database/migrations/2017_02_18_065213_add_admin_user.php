1295db2071055e207cd401c05c719c13e1aaba82
YING CHEN <jun.chenying@hotmail.com>
2017-02-18 21:58:18 +1100

    add admin user

---
+++ database/migrations/2017_02_18_065213_add_admin_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        DB::table('users')->insert([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin')
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
